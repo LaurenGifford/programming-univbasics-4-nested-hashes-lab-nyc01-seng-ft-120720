204a15d1946ffac15fcf0bec52fb497f45e40d5f
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-14 20:23:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,6 +20,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	programmer_hash[:alan_kay][:known_for]
 	
 	programmer_hash = 
  		{
@@ -81,7 +82,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:yukihiro_matsumoto] = {
+ 		    :known_for => "Ruby",
+ 		    :languages => ["LISP", "C"]
+ 		  }
+return programmer_hash
 end
 
 def changing_alan
@@ -103,7 +108,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = alans_new_info
+return programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +131,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
+return programmer_hash
 end
